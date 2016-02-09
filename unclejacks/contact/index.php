e94459b4623913c9b3b591c3fc2aed689d1ee7e6
4037580 <Felipe@MacBook.local>
2016-02-09 08:25:35 -0700

    Remove unclejacks folder

--- unclejacks/contact/index.php
+++
@@ -1,5 +0,0 @@
-<?php include '../view/header.php'; ?>
-    <header>
-        <h1>Contact</h1>
-    </header>
-<?php include '../view/footer.php'; ?>
--- unclejacks/view/footer.php
+++
@@ -1,9 +0,0 @@
-    <footer>
-      <ul>
-        <li><a href="/" title="Go back to CS 313 Home">Back to CS 313 Home</a></li>
-        <li class="copyright">&copy; <?php echo date("Y"); ?> Uncle Jack's Trading Post</li>
-      </ul>
-    </footer>
-  </div>
-</body>
-</html>
