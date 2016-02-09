ac7d0677bf3bb40af27481536477b450011f4656
4037580 <Felipe@MacBook.local>
2016-02-09 08:26:17 -0700

    Remove view folder

--- view/footer.php
+++
@@ -1,5 +0,0 @@
-<footer>This is a BYU-I student project for CS 313: Web Engineering II<br>
-  Copyright © 2016 Felipe Gonzalez. All rights reserved
-    </footer>
-</body>
-</html>
--- view/header.php
+++
@@ -1,4 +0,0 @@
-  <header><a href="/">
-    <img src="images/logo.png" alt="logo"></a>
-    <h1>Felipe Gonzalez - CS 313</h1>
-  </header>
