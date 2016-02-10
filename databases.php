04db32c7e7f8014e6a015e062f8186a6c500f9e2
Felipe Gonzalez <4037580@gmail.com>
2016-02-10 09:27:24 -0700

    database connection

--- databases.php
+++ databases.php
@@ -22,19 +22,3 @@ try {
   echo 'Connection failed';
 }
 }
-
-function unclejacksdb(){
-$server = '127.2.200.130';
-$database = 'unclejacks';
-$username = 'adminen1InPN';
-$password = 'P5X_uMZPdwSl';
-$dsn = 'mysql:host='.$server.';dbname='.$database;
-$options = array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION);
-
-try {
-  $g1db = new PDO($dsn, $username, $password, $options);
-  return $g1db;
-} catch (PDOException $ex) {
-  echo 'Connection failed';
-}
-}
